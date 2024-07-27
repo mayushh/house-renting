2c3fabccf9ca7c64c326bd246a1a9e67ec5cadcc
ayush malviya <ayush.malviya733@gmail.com>
2024-07-28 02:06:44 +0530

    navbar made success

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,29 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=''>
+    <div className='flex  justify-between font-semibold'>
       <div>
       <span className='font-bold'>Real Estate</span> 
       </div>
       <div>
-        <span>
-          home
-        </span>
-        <span>
-          property
-        </span>
-        <span>
-          about
-        </span>
-        <span>
-          contact us
-        </span>
+        <ul className='flex font-semibold'>
+          <li className='mx-4 underline'>
+            Home
+          </li >
+          <li className='mx-4'>
+            Property
+          </li>
+          <li className='mx-4'>
+            About
+          </li>
+          <li className='mx-4'>
+            Contact Us
+          </li>
+        </ul>
       </div>
-      <div>
-        
+      <div className='flex '>
+        <div className='mx-3'>Login</div>
+        <div className='mx-3'>6265414959</div>
       </div>
     </div>
   )
